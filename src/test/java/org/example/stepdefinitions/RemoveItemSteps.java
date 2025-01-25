bac4a8e80716c58b8c35165bf47571aa94a662a9
lindagustafsson <adnil90@gmail.com>
2025-01-25 21:16:44 +0100

    feat(cucumber): add RemoveItemSteps class with cucumber step definitions

---
+++ src/test/java/org/example/stepdefinitions/RemoveItemSteps.java
@@ -0,0 +1,26 @@
+package org.example.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RemoveItemSteps {
+    @Given("a cart containing {string} {int} and {string} {int}")
+    public void aCartContaining(String item1, int price1, String item2, int price2) {
+        cart = new ShoppingCart();
+        cart.addItem(item1, price1);
+        cart.addItem(item2, price2);
+    }
+
+    @When("I remove {string}")
+    public void iRemove(String name) {
+        cart.removeItem(name);
+    }
+
+    @Then("the total price should now be {int}")
+    public void theTotalPriceShouldNowBe(int expected) {
+        assertEquals(expected, cart.getTotalPrice());
+    }
+}
